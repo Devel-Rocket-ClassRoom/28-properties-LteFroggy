15321a77fba73ec23cc7ffc24c0ab3a375b0ea0d
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-02-26 16:04:31 +0900

    BankAccount 완료

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -1,4 +1,25 @@
 using System;
 
-// README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+BankAccount bankAccount = new BankAccount("123-456", "홍길동");
+
+Console.WriteLine($"계좌번호 : {bankAccount.AccountNumber}");
+Console.WriteLine($"예금주 : {bankAccount.OwnerName}");
+Console.WriteLine($"잔액 : {bankAccount.Balance:N0}원");
+
+int amount = 50000;
+Console.WriteLine($"=== {amount:N0}원 입금 ===");
+bankAccount.Deposit( amount );
+Console.WriteLine($"잔액 : {bankAccount.Balance:N0}원");
+
+amount = 20000;
+Console.WriteLine($"=== {amount:N0}원 출금 ===");
+Console.WriteLine($"출금 성공 : {bankAccount.Withdraw(amount)}");
+Console.WriteLine($"잔액 : {bankAccount.Balance:N0}원");
+
+amount = 100000;
+Console.WriteLine($"=== {amount:N0}원 출금 시도 ===");
+Console.WriteLine($"출금 성공 : {bankAccount.Withdraw(amount)}");
+Console.WriteLine($"잔액 : {bankAccount.Balance:N0}원");
+
+Console.WriteLine();
+Console.WriteLine();
